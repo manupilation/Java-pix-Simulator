63877918c5d4f1141b30db780d404f22e0a4bf7d
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-07-03 17:15:10 -0300

    feat: controlador de pix class

--- src/main/java/com/trybe/simuladordepix/ControladorDePix.java
+++ src/main/java/com/trybe/simuladordepix/ControladorDePix.java
@@ -2,6 +2,9 @@ package com.trybe.simuladordepix;
 
 import java.io.IOException;
 
+/**
+ * Class to control the result to Pix send operation.
+ * */
 public class ControladorDePix {
 
   private final ProcessadorDePix processadorDePix;
@@ -21,6 +24,31 @@ public class ControladorDePix {
    *         o resultado da operação.
    */
   public String aoConfirmarPix(int valor, String chave) {
-    return null; // TODO: Implementar.
+    try {
+      this.processadorDePix.executarPix(valor, chave);
+    } catch (ErroValorNaoPositivo e) {
+      return Mensagens.VALOR_NAO_POSITIVO;
+      
+    } catch (ErroChaveEmBranco e) {
+      return Mensagens.CHAVE_EM_BRANCO;
+      
+    } catch (IOException e) {
+      return Mensagens.ERRO_DE_CONEXAO;
+      
+    } catch (ErroChaveNaoEncontrada e) {
+      return Mensagens.CHAVE_NAO_ENCONTRADA;
+      
+    } catch (ErroInterno e) {
+      return Mensagens.ERRO_INTERNO;
+      
+    } catch (ErroSaldoInsuficiente e) {
+      return Mensagens.SALDO_INSUFICIENTE;
+      
+    } catch (ErroDePix e) {
+ 
+      return Mensagens.ERRO_INTERNO;
+    }
+    
+    return Mensagens.SUCESSO;
   }
 }
